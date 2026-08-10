b475787a00041117b0fcccf6282fcf8a546f772e
shivam chaudhary <shivamchaudhary@shivams-MacBook-Air.local>
2026-08-10 10:36:48 +0530

    Configure environment variables and dependencies

--- sap-purchase-order-project/backend/settings.py
+++ sap-purchase-order-project/backend/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7vx!=_k7ufml-uqootz_#y(zi++xkxyd%q#50v_9*t-i_jiryu'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +129,4 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
